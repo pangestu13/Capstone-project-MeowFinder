a4c4329de683319b2407544b12f8fc0572c6d3ee
pangestu13 <adjiern1112@gmail.com>
2023-12-05 22:38:34 +0700

    upload

---
+++ app/src/main/java/com/Capstone/capstoneproject/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.Capstone.capstoneproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+    }
+}
