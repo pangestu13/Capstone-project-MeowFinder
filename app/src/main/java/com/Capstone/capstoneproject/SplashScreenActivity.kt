83747bd0d38a2eb471f7409df5ca9e8adaede40e
pangestu13 <adjiern1112@gmail.com>
2023-12-11 08:25:42 +0700

    splashscreen

--- app/src/main/java/com/Capstone/capstoneproject/SplashScreenActivity.kt
+++ app/src/main/java/com/Capstone/capstoneproject/SplashScreenActivity.kt
@@ -3,11 +3,9 @@ package com.Capstone.capstoneproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-    //justcoment
+        setContentView(R.layout.activity_splash_screen)
     }
 }
