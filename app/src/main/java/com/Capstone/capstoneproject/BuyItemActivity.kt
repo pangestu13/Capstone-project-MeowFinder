e7ba9b7a83c8669538215bba91c8129c88715967
pangestu13 <adjiern1112@gmail.com>
2023-12-13 22:07:57 +0700

    First Commit

---
+++ app/src/main/java/com/Capstone/capstoneproject/BuyItemActivity.kt
@@ -0,0 +1,11 @@
+package com.Capstone.capstoneproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BuyItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_buy_item)
+    }
+}
