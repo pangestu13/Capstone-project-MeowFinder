994e3169a9f96c1f5c5ddf49df8b972fd01790d8
Haidar Triari Respati <haidartrt10@gmail.com>
2023-12-16 15:29:20 +0700

    LoginActivity CreateaccountActivity

---
+++ app/src/main/java/com/Capstone/createaccount/CreateaccountActivity.kt
@@ -0,0 +1,12 @@
+package com.Capstone.createaccount
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.Capstone.capstoneproject.R
+
+class CreateaccountActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_createaccount)
+    }
+}
