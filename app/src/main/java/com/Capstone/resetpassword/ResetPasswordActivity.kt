3a28779ebf0258f428109f99637917f571adb8b8
Haidar Triari Respati <haidartrt10@gmail.com>
2023-12-17 17:48:15 +0700

    create akun ResetPasswordActivity

---
+++ app/src/main/java/com/Capstone/resetpassword/ResetPasswordActivity.kt
@@ -0,0 +1,12 @@
+package com.Capstone.resetpassword
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.Capstone.capstoneproject.R
+
+class ResetPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reset_password)
+    }
+}
