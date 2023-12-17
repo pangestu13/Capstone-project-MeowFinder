b910199292b6af9ab0ec6dfc2f873002f0b8c17a
pangestu13 <adjiern1112@gmail.com>
2023-12-17 09:43:58 +0700

    Intent

--- app/src/main/java/com/Capstone/capstoneproject/EditProfileActivity.kt
+++ app/src/main/java/com/Capstone/capstoneproject/EditProfileActivity.kt
@@ -1,31 +1,42 @@
 package com.Capstone.capstoneproject
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.Button
-import androidx.core.content.ContentProviderCompat.requireContext
+import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.Capstone.capstoneproject.databinding.ActivityEditProfileBinding
+import kotlinx.coroutines.NonCancellable.start
 
-class EditProfileActivity : AppCompatActivity(), View.OnClickListener {
+class EditProfileActivity : AppCompatActivity() {
 
-    private lateinit var btnIntentku: Button
+    private lateinit var _binding: ActivityEditProfileBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
-
-        btnIntentku =  findViewById(R.id.btn_back_home)
-        btnIntentku.setOnClickListener(this)
-
+        _binding = ActivityEditProfileBinding.inflate(layoutInflater)
+        setupAction(_binding.root)
     }
 
-    override fun onClick(v: View) {
-        when(v.id){
-            R.id.btn_back_home -> run{
-                val intentk = Intent(this, MainActivity::class.java)
-                startActivity(intentk)
+//    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+//                              savedInstanceState: Bundle?
+//    ): View? {
+//        _binding = ActivityEditProfileBinding. inflate(inflater, container, false)
+//        return binding?.root
+//    }
+
+    private fun setupAction(root: ConstraintLayout) {
+        _binding.let { bind ->
+            bind.btnSaveChange.setOnClickListener {
+                startActivity((Intent(this,ProfileFragment::class.java)))
             }
         }
-
     }
+
+
 }
+
