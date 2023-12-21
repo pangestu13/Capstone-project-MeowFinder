1350a05ded6a523b61744cb642900347170d558c
pangestu13 <adjiern1112@gmail.com>
2023-12-21 20:00:45 +0700

    generate Api

--- app/src/main/java/com/Capstone/capstoneproject/ui/menu/AdoptPersianActivity.kt
+++ app/src/main/java/com/Capstone/capstoneproject/ui/menu/AdoptPersianActivity.kt
@@ -2,15 +2,26 @@ package com.Capstone.capstoneproject.ui.menu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.Capstone.capstoneproject.R
+import com.Capstone.capstoneproject.databinding.ActivityAdoptPersianBinding
+import com.Capstone.capstoneproject.databinding.ActivityClassificationBinding
 
 class AdoptPersianActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAdoptPersianBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_adopt_persian)
+        binding = ActivityAdoptPersianBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val actionbar =supportActionBar
         actionbar!!.title = "Adopt"
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+        binding.btnAdoptus.setOnClickListener { adoptus() }
+    }
+    private fun adoptus() {
+        Toast.makeText(this, "This feature is not yet available", Toast.LENGTH_SHORT).show()
     }
 }
